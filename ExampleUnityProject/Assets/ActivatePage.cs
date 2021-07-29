25f208d5ba079c40ea6b5a36f1a9da126180c50e
Dimitra Koukou <dimitraa.kk@gmail.com>
2021-07-29 17:58:51 +0300

    Added cracked items in the first house and fixed second key

--- ExampleUnityProject/Assets/ActivatePage.cs
+++ ExampleUnityProject/Assets/ActivatePage.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ActivatePage : MonoBehaviour
 {
     public GameObject page;
+
+    private bool flag;
     // Start is called before the first frame update
     void Awake()
     {
+        flag = true;
         page.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.enabled)
+        if (this.enabled && flag)
+        {
             page.SetActive(true);
+            flag = false;
+        }
+
+        
     }
 }
