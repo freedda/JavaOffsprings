55b79c2f66eb3db11fd471b75525b71cd128698c
freedda <elfi.daiou@gmail.com>
2021-08-24 16:42:01 +0300

    added courier new font, fixed inventory size, fixed panel, fixed player's position, changed project name

---
+++ JavaOffspringsProject/Assets/GetIntoTheTemple.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+
+public class GetIntoTheTemple : MonoBehaviour
+{
+   private GameObject myPlayer;
+   Vector3 warpPosition = Vector3.zero;
+   private bool isTrigger = false;
+
+
+   private void Update()
+   {
+      myPlayer = GameObject.FindGameObjectWithTag("Player");
+      /*if (warpPosition != Vector3.zero)
+      {
+         myPlayer.transform.position = warpPosition;
+         warpPosition = Vector3.zero;
+      }*/
+      if (isTrigger)
+      {
+         myPlayer.transform.position = new Vector3(178, 41, -151);
+      }
+   }
+
+   void OnTriggerEnter(Collider other){
+
+      if (other.gameObject.CompareTag("Player"))
+      {
+        isTrigger = true;
+        PlayersMovement.instance.flagMove = false; 
+      }
+      
+
+   }
+
+}
