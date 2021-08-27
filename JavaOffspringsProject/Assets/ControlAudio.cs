862fa5e899b182444102c53850036871cfce7fd1
freedda <elfi.daiou@gmail.com>
2021-08-27 16:54:34 +0300

    added monologues for one or two persons and made the final monologue

--- JavaOffspringsProject/Assets/ControlAudio.cs
+++ JavaOffspringsProject/Assets/ControlAudio.cs
@@ -8,63 +8,105 @@ using UnityEngine.UI;
 
 public class ControlAudio : MonoBehaviour
 {
-    private AudioSource m_MyAudioSource;
-
-    private GameObject player;
-
-    private bool m_Play;
-
-    public TextMeshProUGUI textDisplay;
-    
-
-    void Start(){ 
-        m_MyAudioSource = GetComponent<AudioSource>();
-        m_Play = true;
-
-    }
-
-    private void Update()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        StartTheAudio();
-    }
-
-    public void StartTheAudio()
-    { 
+        //Audio for 2 players
+        public AudioSource m_MyAudioSource1;
+        //Audio for 1 player
+        public AudioSource m_MyAudioSource2;
         
-        if ((Mathf.Abs(Vector3.Distance(player.transform.position, this.transform.position)) < 1.5) && m_Play)
-        {
-            m_Play = false; 
-            m_MyAudioSource.Play();
-            StartCoroutine(Type());
+        private GameObject player;
+    
+        private GameObject[] players;
+    
+        private bool m_Play; 
+        
+        private bool flag;
+    
+        public TextMeshProUGUI textDisplay;
+        
+    
+        void Start(){ 
+           
+            flag = true;
+            m_Play = true;
         }
+    
+        private void Update()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (flag)
+            {
+                players = GameObject.FindGameObjectsWithTag("Player");
+                flag = false;
 
-    }
+            }
+            
+            StartTheAudio();
+        }
+    
+        public void StartTheAudio()
+        { 
+            
+            if ((Mathf.Abs(Vector3.Distance(player.transform.position, this.transform.position)) < 1.5) && m_Play)
+            {
+    
+                if (players.Length.Equals(2))
+                {
+                    m_MyAudioSource1.Play();
+                    StartCoroutine(Type1());
+                }
+                else
+                {
+                    m_MyAudioSource2.Play();
+                    StartCoroutine(Type2());
+                }
 
-    //Create a coroutine 
-    IEnumerator Type()
-    {
-      
-            textDisplay.text = "You found me! Your ancestors would be very proud of your being here, but now it's your time to escape with your brother.";
-            yield return new WaitForSeconds(7f);
-            textDisplay.text = "In order to succeed and decode the message they left you, you need to be trained in the language they used: an object-oriented language called Java. ";
-            yield return new WaitForSeconds(9f);
-            textDisplay.text = "Your goal is to find 10 hidden pages in order to read your ancestors message.";
-            yield return new WaitForSeconds(4f);
-            textDisplay.text = "To achieve this you must work together with your brother sharing your knowledge, concentrating and most of all have fun!";
-            yield return new WaitForSeconds(6.5f);
-            textDisplay.text= "Make sure you have read the instructions in < how to play > section before you start. ";
-            yield return new WaitForSeconds(5f);
-            textDisplay.text = "Afterwards, one of you should take the first key and unlock the house in front of me.";
-            yield return new WaitForSeconds(4f);
-            textDisplay.text = "The map and the signs will help you not to get lost on your way. Enjoy your exploration!";
-            yield return new WaitForSeconds(5f);
-            textDisplay.text = "";
-
-    }
-
-   
+                m_Play = false;
+            }
+    
+        }
+    
+        //Create a coroutine for 2 players  
+        IEnumerator Type1()
+        {
+          
+                textDisplay.text = "You found me! Your ancestors would be very proud of your being here, but now it's your time to escape with your brother.";
+                yield return new WaitForSeconds(7f);
+                textDisplay.text = "In order to succeed and decode the message they left you, you need to be trained in the language they used: an object-oriented language called Java. ";
+                yield return new WaitForSeconds(9f);
+                textDisplay.text = "Your goal is to find 10 hidden pages in order to read your ancestors message.";
+                yield return new WaitForSeconds(4f);
+                textDisplay.text = "To achieve this you must work together with your brother sharing your knowledge, concentrating and most of all have fun!";
+                yield return new WaitForSeconds(6.5f);
+                textDisplay.text= "Make sure you have read the instructions in < how to play > section before you start. ";
+                yield return new WaitForSeconds(5f);
+                textDisplay.text = "Afterwards, one of you should take the first key and unlock the house in front of me.";
+                yield return new WaitForSeconds(4f);
+                textDisplay.text = "The map and the signs will help you not to get lost on your way. Enjoy your exploration!";
+                yield return new WaitForSeconds(5f);
+                textDisplay.text = "";
+    
+        }
+    
+       //Create a coroutine for  players  
+        IEnumerator Type2()
+        {
+                textDisplay.text = "You found me! Your ancestors would be very proud of your being here, but now it's your time to escape.";
+                yield return new WaitForSeconds(6f);
+                textDisplay.text = "In order to succeed and decode the message they left you, you need to be trained in the language they used: an object-oriented language called Java. ";
+                yield return new WaitForSeconds(9f);
+                textDisplay.text = "Your goal is to find 10 hidden pages in order to read your ancestors message.";
+                yield return new WaitForSeconds(4f);
+                textDisplay.text = "To achieve this you must work concentrated and most of all have fun!";
+                yield return new WaitForSeconds(5f);
+                textDisplay.text= "Make sure you have read the instructions in < how to play > section before you start. ";
+                yield return new WaitForSeconds(5f);
+                textDisplay.text = "Afterwards, you should take the first key and unlock the house in front of me.";
+                yield return new WaitForSeconds(3.5f);
+                textDisplay.text = "The map and the signs will help you not to get lost on your way. Enjoy your exploration!";
+                yield return new WaitForSeconds(5f);
+                textDisplay.text = "";
+        }
+       
     
    
 }
--- JavaOffspringsProject/Assets/GetIntoTheTemple.cs
+++ JavaOffspringsProject/Assets/GetIntoTheTemple.cs
@@ -2,29 +2,59 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using ExitGames.Client.Photon;
+using TMPro;
 using UnityEngine;
 
 public class GetIntoTheTemple : MonoBehaviour
 {
    private GameObject myPlayer;
+   private GameObject[] players;
    Vector3 warpPosition = Vector3.zero;
    private bool isTrigger = false;
    public GameObject finalPanel;
    [SerializeField] private Animator animator;
+   
+   
+   public TextMeshProUGUI textDisplay;
+   public AudioSource m_MyAudioSource1;
+   public AudioSource m_MyAudioSource2;
+   private bool flag;
+   private bool m_Play;
+
+   private void Start()
+   {
+      flag = true;
+      m_Play = true;
+   }
 
    private void Update()
    {
       myPlayer = GameObject.FindGameObjectWithTag("Player");
-      /*if (warpPosition != Vector3.zero)
+      if (flag)
       {
-         myPlayer.transform.position = warpPosition;
-         warpPosition = Vector3.zero;
-      }*/
-      if (isTrigger)
+         players = GameObject.FindGameObjectsWithTag("Player");
+         flag = false;
+
+      }
+    
+      if (isTrigger && m_Play)
       {
+         m_Play = false;
          myPlayer.transform.position = new Vector3(178, 41, -151);
          finalPanel.SetActive(true);
          animator.SetTrigger("Activate");
+         if (players.Length.Equals(2))
+         {
+            m_MyAudioSource1.Play();
+            StartCoroutine(Type1());
+         }
+         else
+         {
+            m_MyAudioSource2.Play();
+            StartCoroutine(Type2());
+         }
+
+
       }
    }
 
@@ -38,5 +68,36 @@ public class GetIntoTheTemple : MonoBehaviour
       
 
    }
+   
+   
+        //Create a coroutine for 2 players
+   IEnumerator Type1() {
+          
+         textDisplay.text = "You did it! You have succeeded in completing all your quests and have reached your final destination, the Temple!";
+         yield return new WaitForSeconds(7f);
+         textDisplay.text = " The manuscripts you have collected divulge the family secret and are parts of a document revealings your origin.";
+         yield return new WaitForSeconds(7f);
+         textDisplay.text = "What your parents didn't have the time to tell you before they passed away, is that you are the King's offsprings.";
+         yield return new WaitForSeconds(6f);
+         textDisplay.text = " Now you are successors, of the Kingdom of Java!!";
+         yield return new WaitForSeconds(3f);
+         textDisplay.text = "";
+         
+   }
+   
+   IEnumerator Type2() {
+          
+      textDisplay.text = "You did it! You have succeeded in completing all your quests and have reached your final destination, the Temple!";
+      yield return new WaitForSeconds(7f);
+      textDisplay.text = " The manuscripts you have collected divulge the family secret and are parts of a document revealings your origin.";
+      yield return new WaitForSeconds(7f);
+      textDisplay.text = "What your parents didn't have the time to tell you before they passed away, is that you are the King's offspring.";
+      yield return new WaitForSeconds(6f);
+      textDisplay.text = " Now you are successor, of the Kingdom of Java!!";
+      yield return new WaitForSeconds(3f);
+      textDisplay.text = "";
+         
+   }
+    
 
 }
