b436da1a1e0e72f613b90c93aa64a62f1748d3d6
Dimitra Koukou <dimitraa.kk@gmail.com>
2021-08-30 20:30:14 +0300

    Made changes in the guide girl and how to play panel

--- JavaOffspringsProject/Assets/ControlAudio.cs
+++ JavaOffspringsProject/Assets/ControlAudio.cs
@@ -64,7 +64,7 @@ public class ControlAudio : MonoBehaviour
         IEnumerator Type1()
         {
           
-                textDisplay.text = "You found me! Your ancestors would be very proud of your being here, but now it's your time to escape with your brother.";
+                textDisplay.text = "You found me! Your ancestors would be very proud of you being here, but now it's your time to escape with your brother.";
                 yield return new WaitForSeconds(7f);
                 textDisplay.text = "In order to succeed and decode the message they left you, you need to be trained in the language they used: an object-oriented language called Java. ";
                 yield return new WaitForSeconds(9f);
@@ -72,7 +72,7 @@ public class ControlAudio : MonoBehaviour
                 yield return new WaitForSeconds(4f);
                 textDisplay.text = "To achieve this you must work together with your brother sharing your knowledge, concentrating and most of all have fun!";
                 yield return new WaitForSeconds(6.5f);
-                textDisplay.text= "Make sure you have read the instructions in < how to play > section before you start. ";
+                textDisplay.text= "Make sure you have read the instructions in \"how to play\" section before you start. ";
                 yield return new WaitForSeconds(5f);
                 textDisplay.text = "Afterwards, one of you should take the first key and unlock the house in front of me.";
                 yield return new WaitForSeconds(4f);
@@ -85,7 +85,7 @@ public class ControlAudio : MonoBehaviour
        //Create a coroutine for  players  
         IEnumerator Type2()
         {
-                textDisplay.text = "You found me! Your ancestors would be very proud of your being here, but now it's your time to escape.";
+                textDisplay.text = "You found me! Your ancestors would be very proud of you being here, but now it's your time to escape.";
                 yield return new WaitForSeconds(6f);
                 textDisplay.text = "In order to succeed and decode the message they left you, you need to be trained in the language they used: an object-oriented language called Java. ";
                 yield return new WaitForSeconds(9f);
@@ -93,7 +93,7 @@ public class ControlAudio : MonoBehaviour
                 yield return new WaitForSeconds(4f);
                 textDisplay.text = "To achieve this you must work concentrated and most of all have fun!";
                 yield return new WaitForSeconds(5f);
-                textDisplay.text= "Make sure you have read the instructions in < how to play > section before you start. ";
+                textDisplay.text= "Make sure you have read the instructions in \"how to play\" section before you start. ";
                 yield return new WaitForSeconds(5f);
                 textDisplay.text = "Afterwards, you should take the first key and unlock the house in front of me.";
                 yield return new WaitForSeconds(3.5f);
